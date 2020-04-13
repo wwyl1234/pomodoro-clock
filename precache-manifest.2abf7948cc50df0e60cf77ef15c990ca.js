008f4731258de14dedff5a8112caee792eb9360e
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-04-13 15:29:36 -0400

    Updates

---
+++ precache-manifest.2abf7948cc50df0e60cf77ef15c990ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c6cc0797e1a606e72bce5b2fd5245d",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "da5714884b5eb5b945d1",
+    "url": "/pomodoro-clock/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e1c15037fa51e80f041e",
+    "url": "/pomodoro-clock/static/js/2.e7829b4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.e7829b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5714884b5eb5b945d1",
+    "url": "/pomodoro-clock/static/js/main.63be63a6.chunk.js"
+  },
+  {
+    "revision": "af4b36dc409d94bbb5a8",
+    "url": "/pomodoro-clock/static/js/runtime-main.4fe24094.js"
+  }
+]);
